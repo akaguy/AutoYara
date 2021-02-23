99d2aa46e37b0632683afd10faccbeabc481496e
Akaguy <gmatzlia@akamai.com>
2021-02-23 14:31:58 +0200

    Remove using paths list file from run_bloom

--- run_bloom.sh
+++ run_bloom.sh
@@ -2,5 +2,5 @@
 ARG3=${3:-0.0001}
 for ngram in 8 16 32 64 128 256 512 1024
 do
-java -Xmx10G -cp target/AutoYara-1.0-SNAPSHOT.jar edu.lps.acs.ml.autoyara.Bytes2Bloom -i "$1" -n $ngram -o "$2" -k $ngram -pl -fp $ARG3
+java -Xmx10G -cp target/AutoYara-1.0-SNAPSHOT.jar edu.lps.acs.ml.autoyara.Bytes2Bloom -i "$1" -n $ngram -o "$2" -k $ngram -fp $ARG3
 done
