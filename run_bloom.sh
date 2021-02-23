9bc8f97c7ae9eff88496a4ded2673992919e9384
Akaguy <gmatzlia@akamai.com>
2021-02-23 20:15:37 +0200

    Add log

--- run_bloom.sh
+++ run_bloom.sh
@@ -5,5 +5,6 @@ JAR="$ARG4/target/AutoYara-1.0-SNAPSHOT.jar"
 echo $JAR
 for ngram in 8 16 32 64 128 256 512 1024
 do
+echo "java -Xmx10G -cp $JAR edu.lps.acs.ml.autoyara.Bytes2Bloom -i $1 -n $ngram -o $2 -k $ngram -fp $ARG3"
 java -Xmx10G -cp $JAR edu.lps.acs.ml.autoyara.Bytes2Bloom -i "$1" -n $ngram -o "$2" -k $ngram -fp $ARG3
 done
